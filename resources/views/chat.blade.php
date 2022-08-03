8b20c656bfde54654f92297cf451582bc542847a
vuducnguyentb <vuducnguyentb@gmail.com>
2022-08-03 21:48:29 +0700

    use vuejs with laravel

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -8,18 +8,29 @@
     <title>Document</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <style>
+        .list-group {
+            overflow-y: scroll;
+            height: 200px;
+        }
+    </style>
 </head>
 <body>
 <div class="container">
     <div class="row" id="app">
-            <ul class="list-group offset-4 col-4">
-                <li class="list-group-item active">Chat Room</li>
-                <li class="list-group-item">Dapibus ac facilisis in</li>
-                <li class="list-group-item">Morbi leo risus</li>
-                <li class="list-group-item">Porta ac consectetur ac</li>
-                <li class="list-group-item">Vestibulum at eros</li>
-                <input type="text" class="form-control" placeholder="Type your message...">
+        <div class="offset-4 col-4">
+            <li class="list-group-item active">Chat Room</li>
+            <ul class="list-group ">
+                <Message v-for="value in chat.messages">
+                    @{{ value }}
+                </Message>
             </ul>
+
+            <input type="text" class="form-control"
+                   placeholder="Type your message..."
+                   v-model="message" @keyup.enter="send"
+            >
+        </div>
     </div>
 </div>
 
