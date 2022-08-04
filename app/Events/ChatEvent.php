1cc49fe303d0b2a174c18f5afdf93df2f92dbd68
vuducnguyentb <vuducnguyentb@gmail.com>
2022-08-04 21:50:58 +0700

    Event to Broadcast

---
+++ app/Events/ChatEvent.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Events;
+
+use App\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class ChatEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+    public $user;
+
+    public function __construct($message,User $user)
+    {
+        $this->message = $message;
+        $this->user = $user;
+    }
+
+    public function broadcastOn()
+    {
+        return new PrivateChannel('chat');
+    }
+}
